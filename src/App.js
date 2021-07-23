158d990b622e8050ca903ff6d02d83fbb829f77a
Suvrodeep <suvrodeephalder@gmail.com>
2021-07-24 02:01:35 +0530

    Almost1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { Button , FormControl, InputLabel, Input  } from '@material-ui/core';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState('');
+  const [messages,setMessages] = useState (['hello' , 'hi' , 'WhatsUp']);
+
+  console.log(input);
+  console.log(messages);
+  
+  const sendMessages = (event) => {
+    //all the logic to send messages goes here . . .
+    event.preventDefault(); //so that jabbhi enter press krta hun, the message gets reloaded on the screen. . .
+    setMessages([...messages , input]); // using spread operator so that message cont. rhe purane array se ...
+    setInput('');
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Hello Suvrodeep! </h1>
+      
+    <form>   
+
+       <FormControl>
+           <InputLabel >Enter a Message . . .</InputLabel>
+           <Input value={input} onChange = {event => setInput(event.target.value)} />
+           <Button disabled={!input}  variant="contained" color="secondary"  type="submit" onClick={sendMessages}> Send Messages </Button> 
+          
+       </FormControl>
+
+    </form>
+
+     {/* Messages Themselves */}
+     
+     {
+       messages.map(message => (
+         <p>{message}</p>
+       ))
+     }
+      
     </div>
   );
 }
